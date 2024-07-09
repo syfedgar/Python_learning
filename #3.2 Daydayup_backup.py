79c61db7d58f409017972808f57e37b018f9d79b
syfedgar <syfedgar@hotmail.com>
2024-07-09 15:53:31 +0800

    Chapter3.2

--- #3.2 Daydayup_backup.py
+++ #3.2 Daydayup_backup.py
@@ -1,30 +1,5 @@
-# 天天向上的力量
-# print(1.001**365)
-# print(0.999**365)
-
-# # #daydayup Q1
-# dayup = pow(1.001, 365)
-# daydown = pow(0.999, 365)
-# print("向上:{:.2f}, 向下{:.2f}".format(dayup,daydown))
-
-
-# # daydayup Q2
-# dayfactor = 0.01
-# dayup =pow(1+dayfactor, 365)
-# daydown = pow(1-dayfactor,365)
-# print("向上:{:.2f}, 向下{:.2f}".format(dayup,daydown))
-
-# daydayup Q3 workday +1%, weekend -1%
-# dayup = 1.0
-# dayfactor = 0.01
-# for i in range(365):
-#     if i % 7 in [6,0]:
-#         dayup = dayup*(1-dayfactor)
-#     else:
-#         dayup = dayup*(1+dayfactor)
-# print("工作日的力量:{:.2f}".format(dayup))
-
-#daydayup Q4 workday +?%, weekend -1%
+#Daydayup_Final
+ 
 
 def dayUP(df):
     dayup = 1
@@ -38,12 +13,6 @@ dayfactor = 0.01
 while dayUP(dayfactor) < 37.78:
     dayfactor += 0.001
 print("工作日努力的参数是:{:.3f}".format(dayfactor))
-
-
-
-# GRIT: Perserverance and passion for long-term goals
-#       坚毅，对长期目标的持续激情及持久耐力；是获得成功最重要的因素之一；
-
 
 # def dayUP(df):  
 #     dayup = 1  
